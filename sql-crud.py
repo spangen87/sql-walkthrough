a94b7763e55f6cb978b41e14e631f0c027850a85
spangen87 <rikard.spangmyr@bestlas.se>
2022-09-15 12:13:09 +0000

    Final commit

--- sql-crud.py
+++ sql-crud.py
@@ -12,7 +12,7 @@ base = declarative_base()
 
 # create a class-based model for the "Programmer" table
 class Programmer(base):
-    __tablename__= "Programmer"
+    __tablename__ = "Programmer"
     id = Column(Integer, primary_key=True)
     first_name = Column(String)
     last_name = Column(String)
@@ -95,10 +95,48 @@ rikard_spangmyr = Programmer(
 # session.add(margaret_hamilton)
 # session.add(bill_gates)
 # session.add(tim_berners_lee)
-session.add(rikard_spangmyr)
+# session.add(rikard_spangmyr)
 
 # commit our session to the database
-session.commit()
+# session.commit()
+
+
+# updating a single record
+# programmer = session.query(Programmer).filter_by(id=7).first()
+# programmer.famous_for = "World President"
+
+# commit our session to the database
+# session.commit()
+
+# updating multiple records
+# people = session.query(Programmer)
+# for person in people:
+#     if person.gender == "F":
+#         person.gender = "Female"
+#     elif person.gender == "M":
+#         person.gender = "Male"
+#     else:
+#         print("Gender not defined")
+#     session.commit()
+
+
+# deleting a single record
+# fname = input("Enter a first name: ")
+# lname = input("Enter a last name: ")
+# programmer = session.query(Programmer).filter_by(first_name=fname, last_name=lname).first()
+# defensive programming
+# if programmer is not None:
+#     print("Programmer Found: ", programmer.first_name + " " + programmer.last_name)
+#     confirmation = input("Are you sure you want to delete this record? (y/n) ")
+#     if confirmation.lower() == "y":
+#         session.delete(programmer)
+#         session.commit()
+#         print("Programmer has been deleted")
+#     else:
+#         print("Programmer not deleted")
+# else:
+#     print("No records found")
+
 
 
 # query the database
